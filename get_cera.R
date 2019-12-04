ed37e33dcb4af44a40c04a21624b985c56ceb0ab
Nick Gauthier <ngauthier@email.arizona.edu>
2019-12-03 17:59:17 -0700

    replace era5 with cera-20c

---
+++ get_cera.R
@@ -0,0 +1,29 @@
+
+# devtools::install_github('khufkens/ecmwfr')
+library(ecmwfr)
+
+# set a key to the keychain
+wf_set_key(user = "ngauthier91@gmail.com",
+           key = "d0b10ca0286891178d1f4f672e2f96eb",
+           service = "webapi")
+
+prec_request <- list(
+  area    = "70/-130/30/-60",
+  class   = "ep",
+  dataset = "cera20c",
+  date    = paste0(paste0(rep(1901:2010, each = 12), paste0(c(paste0(0, 1:9), 10:12),'01')), collapse = '/'),
+  expver  = "1",
+  grid    = "2.0/2.0",
+  levtype = "sfc",
+  number  = "0/1/2/3/4/5/6/7/8/9",
+  param   = "228.128",
+  stream  = "edmo",
+  type    = "fc",
+  format  = "netcdf",
+  target  = "CERA-20C_precipitation.nc"
+)
+
+wf_request(request = prec_request, user = "ngauthier91@gmail.com",
+           transfer = TRUE, path = "data/")
+
+
