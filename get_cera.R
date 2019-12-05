5607913520b946d033fbfab86913c35d01f0e3e1
Nick Gauthier <ngauthier@email.arizona.edu>
2019-12-05 11:08:51 -0700

    messy cca, commit before refactor

--- get_cera.R
+++ get_cera.R
@@ -24,6 +24,23 @@ prec_request <- list(
 )
 
 wf_request(request = prec_request, user = "ngauthier91@gmail.com",
-           transfer = TRUE, path = "data/")
+           transfer = TRUE, path = "data")
 
+snow_request <- list(
+  area    = "70/-130/30/-60",
+  class   = "ep",
+  dataset = "cera20c",
+  date    = paste0(paste0(rep(1901:2010, each = 12), paste0(c(paste0(0, 1:9), 10:12),'01')), collapse = '/'),
+  expver  = "1",
+  grid    = "1.0/1.0",
+  levtype = "sfc",
+  number  = "0/1/2/3/4/5/6/7/8/9",
+  param   = "33.128/141.128",
+  stream  = "edmo",
+  format  = "netcdf",
+  target  = "CERA-20C_snow.nc"
+)
+
+wf_request(request = snow_request, user = "ngauthier91@gmail.com",
+           transfer = TRUE, path = "data")
 
